51959aa438369710c9cdc89a85997a02c27b8299
zxcvvv1010 <zxcvvv1010@163.com>
2018-10-06 15:56:21 +0800

    zero

---
+++ Wpftest1/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//添加用于Socket的类
+using System.Net;
+using System.Net.Sockets;
+
+namespace Wpftest1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Socket c = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);   //  创建Socket
+        Boolean cd=false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonct_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+                try
+                {
+                    int port = 4001;
+                    string host = "192.168.0.178";
+                    //创建终结点EndPoint
+                    IPAddress ip = IPAddress.Parse(host);
+                    IPEndPoint ipe = new IPEndPoint(ip, port);   //把ip和端口转化为IPEndPoint的实例
+
+                    //创建Socket并连接到服务器
+
+                    Console.WriteLine("Connecting...");
+                    c.Connect(ipe); //连接到服务器
+                
+
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine("argumentNullException:{0}", ae);
+                }
+                catch (SocketException ae)
+                {
+                    Console.WriteLine("SocketException:{0}", ae);
+                }
+            
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //向服务器发送信息
+
+                byte[] bs = { 0xFE, 0xFD, 0x0, 0x8, 0x0, 0x0, 0x0, 0x1, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x0, 0x0, 0x0, 0x0a, 0xFE, 0xFD, 0x0, 0x8, 0x0, 0x0, 0x0, 0x1, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x7, 0x7, 0x0, 0x0, 0x0, 0x0b }; //FE - FD - 0 - 8 - 0 - 0 - 0 - 1 - 0 - 1 - 2 - 3 - 4 - 5 - 6 - 7 - F2 - E2 - 28 - 32
+
+                string oo = "";
+                foreach (byte r in bs)
+                {
+
+                    oo = oo + "-" + r.ToString("X");
+
+                }
+                Console.WriteLine("{0}", oo);    //回显服务器的返回信息
+                Console.WriteLine("Send message");
+                c.Send(bs, bs.Length, 0); //发送信息到服务器端
+        }
+             catch (ArgumentException ae)
+            {
+                Console.WriteLine("argumentNullException:{0}", ae);
+            }
+            catch (SocketException ae)
+            {
+                Console.WriteLine("SocketException:{0}", ae);
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("Recieving message");
+                byte[] recvBytes = new byte[1000];
+                int bytes;
+                bytes = c.Receive(recvBytes, recvBytes.Length, 0);    //从服务器端接受返回信息
+                                                                      //recvStr += Encoding.ASCII.GetString(recvBytes, 0, bytes);
+                Console.WriteLine("client get message");    //回显服务器的返回信息
+                string oo = "";
+                foreach (byte r in recvBytes)
+                {
+
+                    oo = oo + "-" + r.ToString("X");
+
+                }
+                Console.WriteLine("{0}", oo);    //回显服务器的返回信息
+
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine("argumentNullException:{0}", ae);
+            }
+            catch (SocketException ae)
+            {
+                Console.WriteLine("SocketException:{0}", ae);
+            }
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Title = e.GetPosition(this).ToString();
+        }
+    }
+}
---
+++ Wpftest1/TcpClientSocket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//添加用于Socket的类
+using System.Net;
+using System.Net.Sockets;
+
+namespace Wpftest1
+{
+    class TcpClientSocket
+    {
+        int port;
+        string host;
+        public TcpClientSocket(int inport, string inhost)
+        {
+            port = inport;
+            host = inhost;
+        }
+        public Socket connect()
+        {
+
+
+            //创建终结点EndPoint
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint ipe = new IPEndPoint(ip, port);   //把ip和端口转化为IPEndPoint的实例
+
+            //创建Socket并连接到服务器
+            Socket c = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);   //  创建Socket
+            Console.WriteLine("Connecting...");
+            c.Connect(ipe); //连接到服务器
+            return c;
+        }
+
+
+        
+    }
+    
+}
