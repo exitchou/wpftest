db6b253c1e33c4cb39a7b0ee68d4ce005a315bcc
zxc <zxcvvv1010@163.com>
2018-10-10 11:31:43 +0800

    可以再次连接

--- Wpftest1/MainWindow.xaml.cs
+++ Wpftest1/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Timers;
 using System.Windows.Threading;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -25,27 +26,37 @@ namespace Wpftest1
     /// </summary>
     public partial class MainWindow : Window
     {
-        DispatcherTimer dispatcherTimer = new DispatcherTimer();
-
+        private System.Timers.Timer aTimer = null;
+        private delegate void TimerDispatcherDelegate();
         const int port = 4001;
         const string host = "192.168.0.178";
+        bool socketConnected;
         byte canBS0, canBS1;
-        byte[] bs = { 0xFE, 0xFD, 0x0, 0x8, 0x0, 0x0, 0x0, 0x1, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x0, 0x0, 0x0, 0x0a }; //FE - FD - 0 - 8 - 0 - 0 - 0 - 1 - 0 - 1 - 2 - 3 - 4 - 5 - 6 - 7 - F2 - E2 - 28 - 32
+        byte[] bs = { 0xFE, 0xFD, 0x0, 0x8, 0x0, 0x0, 0x0, 0x1, 0x0, 0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0a }; //FE - FD - 0 - 8 - 0 - 0 - 0 - 1 - 0 - 1 - 2 - 3 - 4 - 5 - 6 - 7 - F2 - E2 - 28 - 32
         TcpClientSocket client = new TcpClientSocket(port, host);   //  创建TcpClientSocket
                                                                     //TcpClient client = new TcpClient();
-        Thread tR = new Thread(new ParameterizedThreadStart(sockRecieve));
+        Thread tR;
 
         public MainWindow()
         {
             InitializeComponent();
+            socketConnected = false;
             //定时查询-定时器
-            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(100);//100ms
-            dispatcherTimer.Start();
+            aTimer = new System.Timers.Timer(100);
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 100;//100ms
+
         }
-        private void dispatcherTimer_Tick(object sender, EventArgs e)
-        {
-            if ((string)labelConnected1.Content == "#FF3EC30D") sendSocket();//执行查询
+
+                private void OnTimedEvent(object sender, EventArgs e)
+         {
+             this.Dispatcher.Invoke(DispatcherPriority.Normal, new TimerDispatcherDelegate(updateUI));
+            
+        }
+ 
+         private void updateUI()
+         {
+            if (socketConnected) sendSocket();//执行查询
         }
 
         private void sendSocket()
@@ -57,64 +68,32 @@ namespace Wpftest1
 
                     if (checkBox1.IsChecked == true) canBS0 |= 1;
                     else canBS0 &= 0xfe;
-
-
-
                     if (checkBox2.IsChecked == true) canBS0 |= 2;
                     else canBS0 &= 0xfd;
-
-
-
                     if (checkBox3.IsChecked == true) canBS0 |= 4;
                     else canBS0 &= 0xfb;
-
-
                     if (checkBox4.IsChecked == true) canBS0 |= 8;
                     else canBS0 &= 0xf7;
-
-
                     if (checkBox5.IsChecked == true) canBS0 |= 16;
                     else canBS0 &= 0xef;
-
-
                     if (checkBox6.IsChecked == true) canBS0 |= 32;
                     else canBS0 &= 0xdf;
-
-
                     if (checkBox7.IsChecked == true) canBS0 |= 64;
                     else canBS0 &= 0xbf;
-
-
                     if (checkBox8.IsChecked == true) canBS0 |= 128;
                     else canBS0 &= 0x7f;
-
-
                     if (checkBox9.IsChecked == true) canBS1 |= 1;
                     else canBS1 &= 0xfe;
-
-
-
                     if (checkBox10.IsChecked == true) canBS1 |= 2;
                     else canBS1 &= 0xfd;
-
-
-
                     if (checkBox11.IsChecked == true) canBS1 |= 4;
                     else canBS1 &= 0xfb;
-
-
                     if (checkBox12.IsChecked == true) canBS1 |= 8;
                     else canBS1 &= 0xf7;
-
-
                     if (checkBox13.IsChecked == true) canBS1 |= 16;
                     else canBS1 &= 0xef;
-
-
                     if (checkBox14.IsChecked == true) canBS1 |= 32;
                     else canBS1 &= 0xdf;
-
-
                     if (checkBox15.IsChecked == true) canBS1 |= 64;
                     else canBS1 &= 0xbf;
                     if (checkBox16.IsChecked == true) canBS1 |= 128;
@@ -157,30 +136,47 @@ namespace Wpftest1
         private void buttonct_Click(object sender, RoutedEventArgs e)
         {
 
-
-            try
+            if(socketConnected)
             {
-
-                //创建Socket并连接到服务器
-
-                Console.WriteLine("Connecting...");
-                client.Connect(); //连接到服务器
-                                  //Thread tC=new Thread(new ThreadStart(client.Connect));
-                                  // tC.Start();
-                this.labelConnected1.Content = "#FF3EC30D";
-
-                //tR.Start(client);
-
-
+                tR.Abort(client);
+                client.socketOne.Shutdown(SocketShutdown.Both);
+                client.socketOne.Disconnect(true);
+                buttonct.Content = "Connect";
+                socketConnected = false;
+                this.labelConnected1.Content = "#FFC73B3B";
             }
-            catch (ArgumentException ae)
-            {
-                Console.WriteLine("argumentNullException:{0}", ae);
-            }
-            catch (SocketException ae)
-            {
-                Console.WriteLine("SocketException:{0}", ae);
-            }
+            else
+                try
+                {
+
+                    //连接到服务器
+
+                    Console.WriteLine("Connecting...");
+                    client.Connect(); //连接到服务器
+                                      //Thread tC=new Thread(new ThreadStart(client.Connect));
+                                      // tC.Start();
+                    socketConnected = true;
+                    this.labelConnected1.Content = "#FF3EC30D";
+                    buttonct.Content = "Disconnect";
+                    aTimer.Start();
+                    if (socketConnected)
+                    {
+                        tR = new Thread(new ParameterizedThreadStart(sockRecieve));
+                        tR.Start(client);
+                    }
+
+                    //tR.Start(client);
+
+
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine("argumentNullException:{0}", ae);
+                }
+                catch (SocketException ae)
+                {
+                    Console.WriteLine("SocketException:{0}", ae);
+                }
 
         }
 
@@ -301,104 +297,22 @@ namespace Wpftest1
         }
 
 
-        private void labelConnected_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            try
-            {
-                if ((string)this.labelConnected1.Content == "#FF3EC30D") tR.Start(client);
-                // else tR.Abort();
-            }
-            catch (ArgumentException ae)
-            {
-                Console.WriteLine("argumentNullException:{0}", ae);
-            }
-        }
 
-        private void checkBox1_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox1.IsChecked == true) canBS0 |= 1;
-            else canBS0 &= 0xfe;
-        }
 
-        private void checkBox2_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox2.IsChecked == true) canBS0 |= 2;
-            else canBS0 &= 0xfd;
-        }
 
-        private void checkBox3_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox3.IsChecked == true) canBS0 |= 4;
-            else canBS0 &= 0xfb;
-        }
-        private void checkBox4_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox4.IsChecked == true) canBS0 |= 8;
-            else canBS0 &= 0xf7;
-        }
-        private void checkBox5_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox5.IsChecked == true) canBS0 |= 16;
-            else canBS0 &= 0xef;
-        }
-        private void checkBox6_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox6.IsChecked == true) canBS0 |= 32;
-            else canBS0 &= 0xdf;
-        }
-        private void checkBox7_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox7.IsChecked == true) canBS0 |= 64;
-            else canBS0 &= 0xbf;
-        }
-        private void checkBox8_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox8.IsChecked == true) canBS0 |= 128;
-            else canBS0 &= 0x7f;
-        }
-        private void checkBox9_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox9.IsChecked == true) canBS1 |= 1;
-            else canBS1 &= 0xfe;
-        }
 
-        private void checkBox10_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox10.IsChecked == true) canBS1 |= 2;
-            else canBS1 &= 0xfd;
-        }
 
-        private void checkBox11_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox11.IsChecked == true) canBS1 |= 4;
-            else canBS1 &= 0xfb;
-        }
-        private void checkBox12_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox12.IsChecked == true) canBS1 |= 8;
-            else canBS1 &= 0xf7;
-        }
-        private void checkBox13_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox13.IsChecked == true) canBS1 |= 16;
-            else canBS1 &= 0xef;
-        }
-        private void checkBox14_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox14.IsChecked == true) canBS1 |= 32;
-            else canBS1 &= 0xdf;
-        }
-        private void checkBox15_Click(object sender, RoutedEventArgs e)
-        {
-            if (checkBox15.IsChecked == true) canBS1 |= 64;
-            else canBS1 &= 0xbf;
-        }
 
-        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            if (client.socketOne.Connected)client.socketOne.Disconnect(true);
+
+            aTimer.Dispose();
+            tR.Abort();
 
         }
-
 
 
         private void checkBox16_Click(object sender, RoutedEventArgs e)
--- Wpftest1/TcpClientSocket.cs
+++ Wpftest1/TcpClientSocket.cs
@@ -18,18 +18,20 @@ namespace Wpftest1
         {
             port = inport;
             host = inhost;
+
+
+
         }
         public void Connect()
         {
 
-
+            //创建Socket
+            socketOne = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);   //  创建Socket
             //创建终结点EndPoint
             IPAddress ip = IPAddress.Parse(host);
             IPEndPoint ipe = new IPEndPoint(ip, port);   //把ip和端口转化为IPEndPoint的实例
-
-            //创建Socket并连接到服务器
-            socketOne = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);   //  创建Socket
             Console.WriteLine("Connecting...");
+
             socketOne.Connect(ipe); //连接到服务器
         }
 
